ac1f7f0846128b4e21125892300f568d862d40a8
yootaek <eldbxor@naver.com>
2022-06-14 14:40:27 +0900

    Todo 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Todos from './containers/Todos';
 import Counter from './coponents/Counter';
 
 const App = () => {
   return (
     <div>
       <Counter number={0} />
+      <hr />
+      <Todos />
     </div>
   );
 };
